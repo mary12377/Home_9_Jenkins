d077ac63f2d37db1a33b5f39533beca665c1fe4a
Mikhail <bulanjova@mail.ru>
2022-05-20 19:02:28 +0300

    add

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -2,8 +2,10 @@ package demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -11,6 +13,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+    static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     static void setUp() {
@@ -18,7 +21,7 @@ public class TestBase {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = ("https://"+ config.login() + ":" + config.password() + "@" + System.getProperty("urlSelenoid"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
--- src/test/java/properties/OwnerTests.java
+++ src/test/java/properties/OwnerTests.java
@@ -6,18 +6,5 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class OwnerTests {
-    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
-    @Test
-    @Tag("owner")
-    void loginTest() {
-        String login = config.login();
-        String password = config.password();
-
-        System.out.println("Login: " + login);
-        System.out.println("Password: " + password);
-
-        String message = "I logged in as " + login + " with password " + password;
-        System.out.println(message);
-    }
 }
